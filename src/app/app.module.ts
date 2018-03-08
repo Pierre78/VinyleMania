b5cde8b6191e71bbaa439f9bc00007f5b4416a05
Pierre <pierrethuillier77@gmail.com>
2018-03-08 16:31:48 +0100

    Add all my files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { NgModule, ApplicationRef } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NewsComponent } from './news/news.component';
 import { PleasureComponent } from './pleasure/pleasure.component';
 import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes =[
+  {
+    path: 'nouveautés', component: NewsComponent
+  },
+  {
+    path: 'plaisir', component: PleasureComponent
+  },
+  {
+    path: '', component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -16,14 +29,16 @@ import { AccountComponent } from './account/account.component';
     NewsComponent,
     PleasureComponent,
     AccountComponent,
+    HomeComponent,
 
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     CommonModule,
     FormsModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAFzbMfELaaTqu_qDPGv7n7-7IGIAIcOWE'
+      apiKey: '  bda4ada8694db06efcac9cf97b872b3e'
     })
   ],
   providers: [],
